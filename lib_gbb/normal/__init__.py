f7582ede72d0c6174bad52f9827001e93d20afee
Daniel <you@example.com>
2020-05-14 13:32:43 +0200

    update

--- lib_gbb/normal/__init__.py
+++ lib_gbb/normal/__init__.py
@@ -1,5 +1,4 @@
 from .get_normal import get_normal
-from .get_normal_direction import get_normal_direction
 from .plot_normal import plot_normal
 from .plot_normal_direction import plot_normal_direction
 from .plot_white2pial import plot_white2pial
