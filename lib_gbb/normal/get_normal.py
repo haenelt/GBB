d3ab4b584506bfb3999ad33953a96a8f4c6a9869
haenelt <haenelt@cbs.mpg.de>
2020-08-11 12:52:50 +0200

    change face2vertex conversion

--- lib_gbb/normal/get_normal.py
+++ lib_gbb/normal/get_normal.py
@@ -2,7 +2,7 @@ def get_normal(vtx, fac):
     """
     This function computes the surfaces normals per vertex from an input surface mesh. The code is 
     taken from https://sites.google.com/site/dlampetest/python/calculating-normals-of-a-triangle-
-    mesh-using-numpy
+    mesh-using-numpy and adapted to my own purposes.
     Inputs:
         *vtx: vertex array.
         *fac: face array.
@@ -11,37 +11,42 @@ def get_normal(vtx, fac):
         
     created by Daniel Haenelt
     Date created: 13-12-2019
-    Last modified: 18-12-2019
+    Last modified: 11-08-2020
     """
     import numpy as np
 
-    # normalize vectors
     def normalize_v3(arr):
         """ Normalize a numpy array of 3 component vectors shape=(n,3) """
         lens = np.sqrt( arr[:,0]**2 + arr[:,1]**2 + arr[:,2]**2 )
-        arr[:,0] /= lens
-        arr[:,1] /= lens
-        arr[:,2] /= lens
-        return arr
+        res = np.zeros_like(arr)
+        res[:,0] = arr[:,0] / lens
+        res[:,1] = arr[:,1] / lens
+        res[:,2] = arr[:,2] / lens
+        return res
+
+    def f2v(length, fac, nt):
+        """ get average vertex-wise normal by adding up all face-wise normals
+        around vertex """
+        nv = np.zeros((length,3))
+        for i in range(len(fac)):       
+            nv[fac[i,0],:] += nt[i,:]
+            nv[fac[i,1],:] += nt[i,:]
+            nv[fac[i,2],:] += nt[i,:]
+        
+        return nv
 
     # initialize array with same type and shape as vtx
     norm = np.zeros( vtx.shape, dtype=vtx.dtype )
 
     # indexed view into the vertex array
-    tris = vtx[fac]    
+    tris = vtx[fac]   
     
-    # calculate the normal for all triangles by taking the cross product of the vectors v1-v0 and
-    # v2-v0 in each triangle
+    # calculate the normal for all triangles by taking the cross product of 
+    # the vectors v1-v0 and v2-v0 in each triangle
     n = np.cross( tris[::,1 ] - tris[::,0]  , tris[::,2 ] - tris[::,0] )
     
-    # n is array of normal per triangle. Now we normalize all normals
-    normalize_v3(n)
-    
-    # now we have a normalized array of normals, one per triangle. We add each vertex in that
-    # triangle to get one normal per vertex.
-    norm[ fac[:,0] ] += n
-    norm[ fac[:,1] ] += n
-    norm[ fac[:,2] ] += n
-    normalize_v3(norm)
+    # calculate vertex-wise normals and normalize
+    norm = f2v(len(vtx), fac, n)
+    norm = normalize_v3(norm)
     
     return norm
