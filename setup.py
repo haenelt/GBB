e0b551e75dcee612a9a45105df3cb8c1c3c14a97
haenelt <haenelt@cbs.mpg.de>
2020-10-05 09:29:34 +0200

    update

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
-from setuptools import setup
+import setuptools
 
-#setup(
-#      name='gbb',
-#      version='0.1.0',
-#      author='Daniel Haenelt',
-#      author_email='haenelt@cbs.mpg.de',
-#      packages=['package_name', 'package_name.test'],
+# version from textfile?????????????????????????????????????????????????????
+# install_requires=['peppercorn'],
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gbb", # Replace with your own username
+    version="0.1.0",
+    author="Daniel Haenelt",
+    author_email="daniel.haenelt@gmail.com",
+    description="Gradient-based boundary (GBB) surface refinement",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haenelt/GBB",
+    licesen="LICENSE", # brauche ich das?????????????????????????????????????
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>=3',
+    )
+
+
+
+
 #      scripts=['bin/script1','bin/script2'],
-#      url='http://pypi.python.org/pypi/PackageName/',
-#      license='LICENSE.txt',
-#      description='',
-#      long_description=open('README.txt').read(),
-#      python_requires='>=3.6',
-#      install_requires=[
-#          "Django >= 1.1.1",
-#          "pytest",
-#          ],
-#      )
