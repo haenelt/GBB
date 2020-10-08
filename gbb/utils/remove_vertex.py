ca0255d530497d7b10914b2476ca27a0757f247d
haenelt <haenelt@cbs.mpg.de>
2020-10-08 14:30:26 +0200

    make face sorting faster

--- gbb/utils/remove_vertex.py
+++ gbb/utils/remove_vertex.py
@@ -3,6 +3,10 @@
 # external inputs
 import numpy as np
 
+# local inputs
+from gbb.utils.get_adjm import get_adjm
+from gbb.neighbor.nn_2d import nn_2d
+
 
 def remove_vertex(vtx, fac, ind_keep):
     """
@@ -21,61 +25,56 @@ def remove_vertex(vtx, fac, ind_keep):
     Last modified: 08-10-2020
     """
 
-    # get new vertices
-    vtx = vtx[ind_keep,:]
-    
-    # get vertex indices of removed vertices
+    # get indices which will be removed
     ind_tmp = np.arange(len(vtx))
     ind_remove = list(set(ind_tmp) - set(ind_keep))
 
+    # get new vertices
+    vtx = vtx[ind_keep,:]
+
     # get new faces
     fac_keep = np.zeros(len(fac))
-    fac_keep += np.in1d(fac[:,0],ind_keep)
-    fac_keep += np.in1d(fac[:,1],ind_keep)
-    fac_keep += np.in1d(fac[:,2],ind_keep)
-    fac_temp = fac[fac_keep == 3,:]
+    fac_keep += np.in1d(fac[:,0], ind_keep)
+    fac_keep += np.in1d(fac[:,1], ind_keep)
+    fac_keep += np.in1d(fac[:,2], ind_keep)
     fac = fac[fac_keep == 3,:]
     
     # reindex faces
-    c_step = 0
-    n_step = [10,20,30,40,50,60,70,80,90,100]
-    for i in range(len(ind_remove)):
+    loop_status = 0
+    loop_length = len(ind_remove)
+    for i in range(loop_length):
         
         # print status
-        counter = np.floor(i / len(ind_remove) * 100).astype(int)
-        if counter == n_step[c_step]:
+        counter = np.floor(i / loop_length * 100)
+        if counter != loop_status:
             print("sort faces: "+str(counter)+" %")
-            c_step += 1
+            loop_status = counter
         
         tmp = fac[fac >= ind_remove[i]] - 1
         fac[fac >= ind_remove[i]] = tmp
 
-    # remove singularities (vertices without faces)
-    fac_old = fac.copy()
-    n_singularity = np.zeros(len(vtx))
-    c_step = 0
-    fac_counter = 0
-    for i in range(len(vtx)):
-        row, col = np.where(fac_old == i)
- 
-        n_singularity[i] = len(row)
-        if not n_singularity[i]:    
-            fac_temp = fac.copy()
-            fac_temp[fac_temp >= fac_counter] = -1
-            fac_temp[fac_temp != -1] = 0
-            fac += fac_temp
-            fac_counter -= 1
- 
-        # update face counter
-        fac_counter += 1
- 
-        # print status
-        counter = np.floor(i / len(vtx) * 100).astype(int)
-        if counter == n_step[c_step]:
-            print("clean vertices: "+str(counter)+" %")
-            c_step += 1
+    # get adjacency matrix
+    adjm = get_adjm(vtx, fac)
 
-    # vertices and indices without singularities
-    vtx = vtx[n_singularity != 0]
+    # remove singularities (vertices without faces)
+    loop_status = 0
+    loop_length = len(vtx)
+    for i in range(loop_length):
+        
+        # print status
+        counter = np.floor(i / loop_length * 100)
+        if counter != loop_status:
+            print("clean faces: "+str(counter)+" %")
+            loop_status = counter
+        
+        n_neighbor = nn_2d(i, adjm, 0)
+        if not len(n_neighbor):
+            
+            # remove vertex
+            vtx = np.delete(vtx, i, 0)
+            
+            # sort faces
+            tmp = fac[fac >= i] - 1
+            fac[fac >= i] = tmp
     
     return vtx, fac
